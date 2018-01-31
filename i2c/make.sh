9d247e1cf9d47febe5cb1a9edf94168df0fa53f9
Mshrimp <chiyuan.ma@outlook.com>
2018-01-31 17:46:10 +0800

    Makefile debug

--- i2c/make.sh
+++ i2c/make.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-make clean && make && make app && make install && make clean
+if [ -z $1 ]
+then
+	CHIP=h3
+else
+	CHIP=$1
+fi
+
+make clean && make chip=$CHIP && make app chip=$CHIP && make install chip=$CHIP && make clean
