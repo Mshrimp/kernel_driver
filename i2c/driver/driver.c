573c621d4be59ce7d0ca7bf8b646cc962a04b13e
Chiyuan.Ma <chiyuan.ma@netview.com>
2018-02-01 15:15:21 +0800

    Type clean

--- i2c/driver/driver.c
+++ i2c/driver/driver.c
@@ -3,7 +3,7 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 
-#include "fm36/fm36.h"
+//#include "fm36/fm36.h"
 
 #define	DRIVER_NAME					"hi3518ev200"
 
@@ -39,24 +39,30 @@ static unsigned int drv_poll(struct file *filp, struct poll_table_struct *table)
 
 static long drv_ioctl(struct file *filp, unsigned int cmd, unsigned long args)
 {
-	int ret = -1;
+    /*
+	 *int ret = -1;
+     */
 	DRV_DEBUG("driver ioctl, cmd = 0x%X", cmd);
 
-	if (_IOC_TYPE(cmd) != FM36_IOC_MAGIC) {
-		DRV_ERROR("ioctl cmd type error, type = %d", _IOC_TYPE(cmd));
-		return -EINVAL;
-	}
+/*
+ *    if (_IOC_TYPE(cmd) != FM36_IOC_MAGIC) {
+ *        DRV_ERROR("ioctl cmd type error, type = %d", _IOC_TYPE(cmd));
+ *        return -EINVAL;
+ *    }
+ *
+ *    if (_IOC_NR(cmd) > FM36_IOC_MAXNR) {
+ *        DRV_ERROR("ioctl cmd nr error, nr = %d", _IOC_NR(cmd));
+ *        return -EINVAL;
+ *    }
+ */
 
-	if (_IOC_NR(cmd) > FM36_IOC_MAXNR) {
-		DRV_ERROR("ioctl cmd nr error, nr = %d", _IOC_NR(cmd));
-		return -EINVAL;
-	}
-
-	ret = fm36_operation(cmd, args);
-	if (ret) {
-		DRV_ERROR("fm36 operation failed");
-		return -EFAULT;
-	}
+    /*
+	 *ret = fm36_operation(cmd, args);
+	 *if (ret) {
+	 *    DRV_ERROR("fm36 operation failed");
+	 *    return -EFAULT;
+	 *}
+     */
 
 	return 0;
 }
@@ -65,7 +71,9 @@ static int drv_open(struct inode *inodp, struct file *filp)
 {
 	DRV_DEBUG("driver open");
 
-	fm36_init();
+    /*
+	 *fm36_init();
+     */
 
     /*
 	 *fm36_gpio_test();
@@ -78,7 +86,9 @@ static int drv_release(struct inode *inodp, struct file *filp)
 {
 	DRV_DEBUG("driver release");
 
-	fm36_uninit();
+    /*
+	 *fm36_uninit();
+     */
 
 	return 0;
 }
